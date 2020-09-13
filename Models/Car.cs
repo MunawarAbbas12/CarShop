1e7cc0db5099d8766aea9de11971de1de2c3f458
Raheem Ullah <raheemullah010101@gmail.com>
2020-09-13 21:16:31 +0500

    Add project files.

---
+++ Models/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarShop.Models
+{
+    public class Car
+    {
+        public int ID { get; set; }
+        public string Company { get; set; }
+        public String Model { get; set; }
+        public int CC { get; set; }
+        public int Price { get; set; }
+    }
+}
